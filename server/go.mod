e05e3cf555468403ffbb2b5bcc50731b0e1fa819
Nathan Bland <nathan.bland@gmail.com>
2021-01-16 01:46:09 -0700

    work towards connecting a mongo adapter and init function

---
+++ server/go.mod
@@ -0,0 +1,13 @@
+module github.com/NathanBland/go-vite-docker-starter
+
+go 1.15
+
+require (
+	github.com/cosmtrek/air v1.21.2 // indirect
+	github.com/creack/pty v1.1.11 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	go.mongodb.org/mongo-driver v1.4.4
+	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,7 +1,28 @@
+// https://www.digitalocean.com/community/tutorials/how-to-use-go-with-mongodb-using-the-mongodb-go-driver
 package main
 
-import "fmt"
+import (
+    "context"
+    "log"
 
-func main() {
-	fmt.Println("Hello go live reload!")
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+var collection *mongo.Collection
+var ctx = context.TODO()
+
+
+func init() {
+    clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+    client, err := mongo.Connect(ctx, clientOptions)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = client.Ping(ctx, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    collection = client.Database("noted").Collection("notes")
 }
