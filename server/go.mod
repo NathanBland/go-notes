742c5216ed678f15fca2b114d583ec385cc40e3a
Nathan Bland <nathan.bland@gmail.com>
2021-01-18 14:12:23 -0700

    Fiber setup with sqllite3

--- server/go.mod
+++ server/go.mod
@@ -6,7 +6,10 @@ require (
 	github.com/cosmtrek/air v1.21.2 // indirect
 	github.com/creack/pty v1.1.11 // indirect
 	github.com/fatih/color v1.10.0 // indirect
+	github.com/gofiber/fiber v1.14.6
+	github.com/gofiber/fiber/v2 v2.3.3
 	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/jinzhu/gorm v1.9.16
 	github.com/pelletier/go-toml v1.8.1 // indirect
 	go.mongodb.org/mongo-driver v1.4.4
 	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78 // indirect
--- server/main.go
+++ server/main.go
@@ -2,27 +2,67 @@
 package main
 
 import (
-    "context"
-    "log"
+	"context"
+	"fmt"
+	"log"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/NathanBland/go-vite-docker-starter/book"
+	"github.com/NathanBland/go-vite-docker-starter/database"
+	"github.com/gofiber/fiber"
+    "github.com/jinzhu/gorm"
+    // _ "github.com/jinzhu/gorm/dialects/sqlite"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
 var collection *mongo.Collection
 var ctx = context.TODO()
 
-
 func init() {
-    clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
-    client, err := mongo.Connect(ctx, clientOptions)
-    if err != nil {
-        log.Fatal(err)
-    }
+	clientOptions := options.Client().ApplyURI("mongodb://mongo-db:27017/")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    err = client.Ping(ctx, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    collection = client.Database("noted").Collection("notes")
+	collection = client.Database("noted").Collection("notes")
+}
+
+func helloWorld(c *fiber.Ctx) {
+	c.Send("hello world")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+	fmt.Println("Database connection successfully established")
+
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+
+	initDatabase()
+	defer database.DBConn.Close()
+
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(":3001"))
 }
